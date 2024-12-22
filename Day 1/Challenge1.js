f2507f1eafd708a84060681ac4006f9d8c1cf3b4
Joel Esteban Diaz Arévalo <jodiazare@gmail.com>
2024-12-21 19:58:33 -0500

    Update Challenge1.js

--- Day 1/Challenge1.js
+++ Day 1/Challenge1.js
@@ -3,13 +3,14 @@
  * @returns {number[]} An array with the prepared gifts
  */
 function prepareGifts(gifts) {
-  // Code here
-  let giftsForSanta = [];
+  // Create an empty array to save the sorted data
+  let giftsForSanta = []
+  // Filter the valid elements and avoid the duplicated ones
   giftsForSanta = gifts.filter((element, index) => {
-    return Number.isInteger(element) && gifts.indexOf(element) === index;
+    return Number.isInteger(element) && gifts.indexOf(element) === index
   });
+  // Sort the elements in ascending order
+  giftsForSanta = giftsForSanta.sort((a, b) => a - b)
 
-  giftsForSanta = giftsForSanta.sort((a, b) => a - b);
-
-  return giftsForSanta;
+  return giftsForSanta
 }
